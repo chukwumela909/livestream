6b24784cbde99ada40cca8a001adf0d18f9f55c2
Amirize Wisdom <97132123+chukwumela909@users.noreply.github.com>
2025-03-15 22:16:04 +0100

    hjhj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,24 +8,22 @@ export default function Home() {
       <Container size="1">
         <Flex direction="column" align="center" gap="5">
           <Image
-            src="/wordmark.svg"
-            alt="LiveKit"
+            src="/logo.png"
+            alt="Wordcast"
             width="240"
             height="120"
             className="invert dark:invert-0 mt-8 mb-2"
           />
-          <Text as="p">
-            Welcome to the LiveKit livestream demo app. You can join or start
-            your own stream. Hosted on{" "}
-            <Link href="https://livekit.io/cloud" target="_blank">
-              LiveKit Cloud
-            </Link>
-            . Bored of the current color theme? Switch it up by pressing{" "}
-            <Kbd>⌘&thinsp;C</Kbd> !
+          <Text className="text-center" as="p">
+            Welcome to Wordcast
           </Text>
+          <Text>
+            Stream the Gospel from around the globe.
+          </Text>
+
           <HomeActions />
           <Separator orientation="horizontal" size="4" className="my-2" />
-          <Text as="p" size="2">
+          {/* <Text as="p" size="2">
             Feel free to clone this full-stack NextJS app{" "}
             <Link
               href="https://github.com/livekit-examples/livestream"
@@ -34,16 +32,8 @@ export default function Home() {
               here
             </Link>
             .
-            {/* And also be sure to check out clone our{" "}
-            <Link
-              href="https://github.com/livekit-examples/swift-livestream"
-              target="_blank"
-            >
-              iOS
-            </Link>{" "}
-            and <Link href="https://github.com/livekit-examples/android-livestream" target="_blank">Android</Link> clients, which are compatible with
-            this web app! */}
-          </Text>
+          
+          </Text> */}
         </Flex>
       </Container>
     </main>
--- src/components/home-actions.tsx
+++ src/components/home-actions.tsx
@@ -10,16 +10,16 @@ export function HomeActions() {
     <Flex direction="column" gap="4" justify="center" align="center">
       <Flex gap="2">
         <BroadcastDialog>
-          <Button size="3">Stream from browser</Button>
+          <Button size="3">Start Live</Button>
         </BroadcastDialog>
         <IngressDialog>
-          <Button size="3">Stream from OBS</Button>
+          <Button size="3">Use Streaming software</Button>
         </IngressDialog>
       </Flex>
       <Text size="1">- OR -</Text>
       <JoinDialog>
         <Button variant="outline" size="3" className="w-full">
-          Join existing stream
+          Join existing live
         </Button>
       </JoinDialog>
     </Flex>
